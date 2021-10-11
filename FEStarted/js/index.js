a82027190e074089cd6bba4bdc22355db9fa56fa
hoangthaitl98 <buihoangthaitl98@gmail.com>
2021-10-11 15:33:08 +0700

    modified ngày 11/10

--- FEStarted/js/index.js
+++ FEStarted/js/index.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-undef */
-$(document).ready(function () {
-    const $body = $('body');
-    $.get('/components/header.html', data => {$body.append(data); $body.append(data)})
+$(document).ready(function() {
+    /* const $body = $('body');
+    $.get('/components/header.html', data => {$body.append(data); $body.append(data)}) */
+    $('.slider').bxSlider();
 });
+
+/* function displayDropDownMobile(){
+    $('.banner-nav-left-btn').append()
+} */
